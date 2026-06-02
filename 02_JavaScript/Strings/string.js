e98d330ebeb9529dc860dc45e89f9419caa7a3af
jaikaran109 <jaik05581@gmail.com>
2026-06-02 23:34:24 +0530

    Create string.js

---
+++ 02_JavaScript/Strings/string.js
@@ -0,0 +1,21 @@
+let name = "jai karan";
+console.log(name,name.length);
+
+
+console.log("________________________________________________");
+
+// with for loop
+for(let i = 0 ; i < name.length ; i++){
+    console.log(name[i]);
+}
+
+
+console.log("________________________________________________");
+
+// with for-of loop
+for(const char of name){
+    console.log(char);
+}
+
+
+console.log("________________________________________________");
