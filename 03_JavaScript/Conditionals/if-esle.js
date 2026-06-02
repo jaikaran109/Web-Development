503b703d5f7e8f9c02c6e8fe0febf877be4e0df1
jaikaran109 <jaik05581@gmail.com>
2026-06-02 20:21:58 +0530

    Create if-esle.js

---
+++ 03_JavaScript/Conditionals/if-esle.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")();
+
+let rating = Number(prompt("Give Rating"));
+
+if(rating === 1) console.log("Poor");
+else if(rating === 2) console.log("Below Average");
+else if(rating === 3) console.log("Average");
+else if(rating === 4) console.log("Good");
+else if(rating === 5) console.log("Exellent");
+else console.log("Invalid Rating");
+
