bbcfd2b3a952ec56344e089ad390ada1c28ee19c
jaikaran109 <jaik05581@gmail.com>
2026-06-03 01:56:14 +0530

    Create mathematicalFunctions.js

---
+++ 02_JavaScript/functions/mathematicalFunctions.js
@@ -0,0 +1,19 @@
+console.log(Math.abs(-7.7));
+
+console.log(Math.max(5,5.7));
+console.log(Math.min(-5,1));
+console.log(Math.max(5,12,45,7,89));
+
+console.log(Math.pow(5,3));
+console.log(Math.pow(3.14,3.14));
+console.log(Math.log(2.82));
+console.log(Math.log10(10));
+
+console.log(Math.sqrt(54.54));
+console.log(Math.cbrt(34554));
+
+console.log(Math.floor(3.7)); // give the Greater Integer
+console.log(Math.floor(-4.5));
+console.log(Math.ceil(4.7)); // return the upper value
+
+console.log(Math.random());    // it generate a real number bw 0 and 1 (excluded)
