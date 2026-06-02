3e9dbf38c4c4981c8897add4063f36d330cb62e8
jaikaran109 <jaik05581@gmail.com>
2026-06-02 22:57:49 +0530

    Create falsyTruthyValues.js

---
+++ 02_JavaScript/Conditionals/falsyTruthyValues.js
@@ -0,0 +1,73 @@
+//Little bit important for interview
+
+
+
+
+
+
+// Falsy Values --> false condition , false , 0 , NaN , undefined
+// Truthy Values --> everything except falsy value
+
+
+// Examples -- 
+if(undefined){
+    console.log("Hello");
+}else{
+    console.log("Mello");
+}
+
+
+if(NaN){
+    console.log("Hello");
+}else{
+    console.log("Mello");
+}
+
+
+if(false){
+    console.log("Hello");
+}else{
+    console.log("Mello");
+}
+
+
+if(0){
+    console.log("Hello");
+}else{
+    console.log("Mello");
+}
+
+
+if(null){
+    console.log("Hello");
+}else{
+    console.log("Mello");
+}
+
+
+if("null"){
+    console.log("Hello");
+}else{
+    console.log("Mello");
+}
+
+
+if(0.7){
+    console.log("Hello");
+}else{
+    console.log("Mello");
+}
+
+
+if(1000){
+    console.log("Hello");
+}else{
+    console.log("Mello");
+}
+
+
+if('false'){
+    console.log("Hello");
+}else{
+    console.log("Mello");
+}
