83a5532e415053bdd0a3351a2e7aedd069f38043
jaikaran109 <jaik05581@gmail.com>
2026-06-07 16:54:26 +0530

    Create basic.js

---
+++ 02_JavaScript/Events/Basics/basic.js
@@ -0,0 +1,10 @@
+// events are the things happen to the html elements..
+// When JavaScript is used in HTML pages, JavaScript can "react" on these events
+
+
+
+// Like click , moveMouse , etc
+// by using this you can change the effects,works and many more things
+
+
+// eg : suppose you have 2 squares and you  want that if you click on box1 then color of box 2 get changes and every time new color then this is possible with the helps of event Listeners
