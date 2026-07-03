429f7a7af7b4362e75d43c9141d6fce47a025746
jaikaran109 <jaik05581@gmail.com>
2026-07-03 05:55:46 +0530

    Delete 02_JavaScript/JS-Phase1 directory

--- 02_JavaScript/JS-Phase1/Backticking/app.js
+++
@@ -1,6 +0,0 @@
-let firstName = "jai karan";
-let lastName = "Gupta";
-
-let fullName = `hi ${firstName} ${lastName} is a student`
-
-console.log(fullName);
--- 02_JavaScript/JS-Phase1/Font-Awesome/script.js
+++
@@ -1,8 +0,0 @@
-// Use google font , kit font etc for custom fonts
-
-console.log(typeof(10));
-console.log(typeof(NaN));
-console.log(typeof(null));
-console.log(typeof(undefined));
-console.log(0/0);
-console.log(10/0);
