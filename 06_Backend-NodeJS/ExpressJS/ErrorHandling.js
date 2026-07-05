34ce8b49944e0742f189c72a3560f905b88bd5d7
jaikaran109 <jaik05581@gmail.com>
2026-07-05 07:34:48 +0530

    Add files via upload

---
+++ 06_Backend-NodeJS/ExpressJS/ErrorHandling.js
@@ -0,0 +1,28 @@
+const express = require('express'); // express package ko import kiya
+const app = express();   // pura express ab ek variable app name me hn
+const port = 3000;
+
+
+
+app.use(function(req , res , next){
+//   console.log("Im the middle ware");      // to see this refresh the profile web page and look at terminal
+  log("Im the middle ware");  // error
+  next();  // to pass next one
+})
+
+
+app.get('/', (req, res) => {      // http://localhost:3000
+  res.send('Hello World !!!!')
+})
+
+   
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
---
+++ 06_Backend-NodeJS/ExpressJS/README.md
@@ -0,0 +1,7 @@
+express - ek npm package hai
+its a freamework - flow/steps of doing something
+it manages everything from receiving the request and giving the response , blog ko json me krna , etc etc
+
+
+
+Middleware - tm kisi bhi route ko chlane se phle kuch activity perform krna chahte ho to usko middle ware bolenge
