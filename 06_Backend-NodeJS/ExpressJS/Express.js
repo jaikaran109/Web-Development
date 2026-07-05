be3522c203a22a67e4ec7f269356fb3f459167aa
jaikaran109 <jaik05581@gmail.com>
2026-07-05 07:34:18 +0530

    Update Express.js

--- 06_Backend-NodeJS/ExpressJS/Express.js
+++ 06_Backend-NodeJS/ExpressJS/Express.js
@@ -1,10 +1,25 @@
-const express = require('express');
-const app = express()
-const port = 3000
+const express = require('express'); // express package ko import kiya
+const app = express();   // pura express ab ek variable app name me hn
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.get('/', (req, res) => {      // http://localhost:3000
+  res.send('Hello World !!!!')
 })
+
+
+
+app.use(function(req , res , next){
+  console.log("Im the middle ware");      // to see this refresh the profile web page and look at terminal
+  next();  // to pass next one
+})
+
+
+
+app.get('/profiles', (req, res) => {      // http://localhost:3000/profiles
+  res.send('arre bhai ye to mera yani jaikaran ka profile h')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
