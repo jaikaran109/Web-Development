0451bdac1afa1e258fd8b587fc4bae154b44f961
jaikaran109 <jaik05581@gmail.com>
2026-07-27 02:31:23 +0530

    Update if-esle.js

--- 02_JavaScript/Conditionals/if-esle.js
+++ 02_JavaScript/Conditionals/if-esle.js
@@ -6,6 +6,6 @@ if(rating === 1) console.log("Poor");
 else if(rating === 2) console.log("Below Average");
 else if(rating === 3) console.log("Average");
 else if(rating === 4) console.log("Good");
-else if(rating === 5) console.log("Exellent");
+else if(rating === 5) console.log("Excellent");
 else console.log("Invalid Rating");
 
